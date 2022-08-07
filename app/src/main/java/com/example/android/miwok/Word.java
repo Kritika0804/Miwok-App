4462538d385fc81541cde97cff79cebc580a8117
Kritika <kritikanc2003@gmail.com>
2022-08-08 00:48:05 +0530

    updated

---
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -0,0 +1,52 @@
+package com.example.android.miwok;
+
+
+
+
+public class Word {
+
+
+    private String mDefaultTranslation;
+
+
+    private String mMiwokTranslation;
+
+//    private int mImageResourceId;
+    /** Image resource ID for the word */
+    private int mImageResourceId = NO_IMAGE_PROVIDED;
+
+    /** Constant value that represents no image was provided for this word */
+    private static final int NO_IMAGE_PROVIDED = -1;
+
+
+
+    public Word(String defaultTranslation, String miwokTranslation) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+    }
+
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mImageResourceId = imageResourceId;
+    }
+
+
+    public String getDefaultTranslation() {
+        return mDefaultTranslation;
+    }
+
+
+    public String getMiwokTranslation() {
+        return mMiwokTranslation;
+    }
+
+    public int getImageResourceId() {
+        return mImageResourceId;
+    }
+
+    public boolean hasImage() {
+        return mImageResourceId != NO_IMAGE_PROVIDED;
+    }
+
+}
